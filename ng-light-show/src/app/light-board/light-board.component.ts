e0b03da62f820f3ced84d3271ccf0965db5c89e5
Ross Arroyo <ross@arroyoDev.com>
2019-01-25 02:23:58 -0600

    added meteor effect

--- ng-light-show/src/app/light-board/light-board.component.ts
+++ ng-light-show/src/app/light-board/light-board.component.ts
@@ -13,7 +13,7 @@ export class LightBoardComponent implements OnInit {
   realTimeBrightness = 255;
   activeColor = 'white';
   activeEffect = 'solid';
-  effects = ['solid', 'bpm', 'candy cane', 'confetti', 'cyclon rainbow', 'dots', 'fire', 'glitter', 'juggle', 'lightning',
+  effects = ['solid', 'meteor', 'bpm', 'candy cane', 'confetti', 'cyclon rainbow', 'dots', 'fire', 'glitter', 'juggle', 'lightning',
    'police all', 'police one', 'rainbow', 'rainbow with glitter', 'sinelon', 'twinkle', 'noise', 'ripple', 'update']
 
   constructor(private lightService: LightService) { }
